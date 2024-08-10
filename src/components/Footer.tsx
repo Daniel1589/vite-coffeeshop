4aaedd6c66f4077208b2025ea74b2abcf729a660
Daniel1589 <daniel1589@edu.aac.ac.il>
2024-08-10 16:41:08 +0300

    Added a complete Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,58 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="bg-sky-100 py-28 flex flex-col">
+      <div className="flex flex-col gap-8 pl-52">
+        <h1 className="text-2xl">COME VISIT US</h1>
+        <ul className="flex flex-col gap-10">
+          <li>500 TERRY FRANCINE STREET SAN FRANCISCO, CA 94158</li>
+          <ul>
+            <li>SUNDAY: 9:00 - 20:00</li>
+            <li>MON - FRI: 8:00 - 20:00</li>
+            <li>SATURDAY: 9:00 - 19:00</li>
+          </ul>
+          <ul className="flex flex-col gap-2">
+            <li>INSTAGRAM</li>
+            <li>FACEBOOK</li>
+            <li>TWITTER</li>
+          </ul>
+        </ul>
+      </div>
+
+      <div className="flex absolute  right-24 flex-col gap-8">
+        <h1 className="text-2xl">TALK TO US</h1>
+
+        <ul className="">
+          <li>INFO@MYSITE.COM</li>
+          <li>050-111-1111</li>
+        </ul>
+
+        <div className="flex flex-col gap-2 pt-20">
+          <p>Subsribe to our Newsletter *</p>
+          <div className="flex">
+            <input
+              className="bg-sky-100 border-black border-2 pl-4 pr-28"
+              type="email"
+              placeholder="Email address"
+            ></input>
+
+            <div className="bg-black text-white px-6 py-3 text-l hover:bg-green-700 duration-500">
+              <Link to="/Subscribe">Subscribe</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center absolute bottom-0 left-0 right-0">
+        <ul className="flex gap-14">
+          <li>TERMS & CONDITIONS</li>
+          <li>PRIVACY POLICY</li>
+          <li>SHIPPING POLICY</li>
+          <li>REFUND POLICY</li>
+          <li>COOKIE POLICY</li>
+        </ul>
+        <p className="py-4">©2069 by BREW.</p>
+      </div>
+    </div>
+  );
 };
